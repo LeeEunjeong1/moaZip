ae1b01ac5809795b50287c38db9ec3ec0051ac7d
eunjeong Lee <tbig1019@gmail.com>
2026-07-26 16:43:53 +0900

    refactor: Hilt 모듈 역할별 분리
    - Firebase, Repository, UseCase 제공 모듈 분리
    - 기존 AppModule 제거

--- android/app/src/main/java/com/moazip/app/di/RepositoryModule.kt
+++ android/app/src/main/java/com/moazip/app/di/RepositoryModule.kt
@@ -4,18 +4,11 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.moazip.core.data.InMemoryDashboardRepository
 import com.moazip.core.domain.auth.CurrentUserProvider
-import com.moazip.core.domain.repository.AuthRepository
 import com.moazip.core.domain.repository.AssetRepository
+import com.moazip.core.domain.repository.AuthRepository
 import com.moazip.core.domain.repository.DashboardRepository
 import com.moazip.core.domain.repository.HouseholdRepository
 import com.moazip.core.domain.repository.UserRepository
-import com.moazip.core.domain.usecase.AddAssetUseCase
-import com.moazip.core.domain.usecase.CreateHouseholdUseCase
-import com.moazip.core.domain.usecase.GetLatestInviteCodeUseCase
-import com.moazip.core.domain.usecase.HasJoinedHouseholdUseCase
-import com.moazip.core.domain.usecase.JoinHouseholdWithInviteCodeUseCase
-import com.moazip.core.domain.usecase.ObserveDashboardSummary
-import com.moazip.core.domain.usecase.ReissueInviteCodeUseCase
 import com.moazip.core.firebase.FirebaseAssetRepository
 import com.moazip.core.firebase.FirebaseAuthRepository
 import com.moazip.core.firebase.FirebaseCurrentUserProvider
@@ -29,15 +22,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
-    @Provides
-    @Singleton
-    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
-
-    @Provides
-    @Singleton
-    fun provideFirebaseFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
-
+object RepositoryModule {
     @Provides
     @Singleton
     fun provideCurrentUserProvider(firebaseAuth: FirebaseAuth): CurrentUserProvider =
@@ -68,32 +53,4 @@ object AppModule {
     @Provides
     @Singleton
     fun provideDashboardRepository(): DashboardRepository = InMemoryDashboardRepository()
-
-    @Provides
-    fun provideCreateHouseholdUseCase(repository: HouseholdRepository) =
-        CreateHouseholdUseCase(repository)
-
-    @Provides
-    fun provideHasJoinedHouseholdUseCase(repository: HouseholdRepository) =
-        HasJoinedHouseholdUseCase(repository)
-
-    @Provides
-    fun provideJoinHouseholdWithInviteCodeUseCase(repository: HouseholdRepository) =
-        JoinHouseholdWithInviteCodeUseCase(repository)
-
-    @Provides
-    fun provideReissueInviteCodeUseCase(repository: HouseholdRepository) =
-        ReissueInviteCodeUseCase(repository)
-
-    @Provides
-    fun provideGetLatestInviteCodeUseCase(repository: HouseholdRepository) =
-        GetLatestInviteCodeUseCase(repository)
-
-    @Provides
-    fun provideAddAssetUseCase(repository: AssetRepository) =
-        AddAssetUseCase(repository)
-
-    @Provides
-    fun provideObserveDashboardSummary(repository: DashboardRepository) =
-        ObserveDashboardSummary(repository)
 }
